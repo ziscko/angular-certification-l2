93a0797a2f91f7a5bf22840b09413b933364945d
Juan Francisco González Reyes <ziscko.team@gmail.com>
2023-10-24 22:35:54 -0600

    Fix team results subscription

--- src/app/components/teams-results/team-results.component.ts
+++ src/app/components/teams-results/team-results.component.ts
@@ -1,20 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { FixtureResponse } from '../../models/fixture.model';
 import { FootballService } from '../../services/football.service';
 import { TeamsResponse } from '../../models/team';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-team-results',
   templateUrl: './team-results.component.html',
   styleUrls: ['./team-results.component.scss']
 })
-export class TeamResultsComponent {
+export class TeamResultsComponent implements OnDestroy {
   teamId: number | undefined;
   teamInfo$: Observable<TeamsResponse[]> | undefined;
   teamFixtures$: Observable<FixtureResponse[]> | undefined;
   loadingError: boolean = false;
+  private destroy$ = new Subject<void>();
 
   constructor(private route: ActivatedRoute, private footballAPI: FootballService) {}
 
@@ -24,23 +26,29 @@ export class TeamResultsComponent {
     this.getResults();
   }
 
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
   getResults(): void {
     if (this.teamId) {
       this.teamFixtures$ = this.footballAPI.getResults(this.teamId, 10);
-      /* this.teamFixtures$.subscribe({
+      this.teamFixtures$.pipe(takeUntil(this.destroy$)).subscribe({
         next: (data) => {
           if (data.length < 1) this.loadingError = true;
         },
         error: () => {
           this.loadingError = true;
         }
-      }); */
+      });
     }
   }
 
   getTeamInfo(): void {
     if (this.teamId) {
       this.teamInfo$ = this.footballAPI.getTeamInfo(this.teamId);
+      this.teamInfo$.pipe(takeUntil(this.destroy$)).subscribe();
     }
   }
 }
